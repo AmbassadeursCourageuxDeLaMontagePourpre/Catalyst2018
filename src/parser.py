edc0a23ee6e859c10e4e9bafdea8d4637ef88067
altor <arthur.dazemar@etudiant.univ-lille1.fr>
2018-04-27 15:23:10 +0200

    parser

---
+++ src/parser.py
@@ -0,0 +1,24 @@
+
+def parse():
+    l1 = list(map(int, input().split()))
+    start = l1[0]
+    end = l1[1]
+    nb_images = l1[2]
+    result = []
+    for image_id in range(nb_images):
+        l1 = list(map(int, input().split()))
+        time_stamp = l1[0]
+        nb_row = l1[1]
+        nb_col = l1[2]
+
+        image = np.zeros((nb_row, nb_col), dtype=int)
+        
+        for row_id in range(nb_row):
+            l = list(map(int, input().split()))
+            for col_id in range(nb_col):
+                image[row_id][col_id] = l[col_id]
+
+        result.append((time_stamp, image))
+    return result
+                
+
