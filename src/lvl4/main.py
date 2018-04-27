8be359923c8a2466bff6b77ace6ace0829e5185b
Jordan <jordan.bouchoucha@ntymail.com>
2018-04-27 19:01:10 +0200

    lvl4 rot

--- src/lvl4/main.py
+++ src/lvl4/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import same_meteorite
 from parser import *
+from rotate import *
 
 images = parse()
 
@@ -9,12 +10,12 @@ db = []
 def insert(img, timestamp, db):
     l = same_meteorite.same_meteroite(img)
     for i in range(len(db)):
-         (img_tuples, all_times) = db[i]
+         (img, img_tuples, all_times) = db[i]
          if l == img_tuples:
              all_times.append(timestamp)
-             db[i] = (img_tuples, all_times)
+             db[i] = (img, img_tuples, all_times)
              return
-    db.append((l, [timestamp]))
+    db.append((img, l, [timestamp]))
     return
 
 #identifier meteorites de meme forme
@@ -30,10 +31,23 @@ def same_shape(images):
             if i == np.shape(img)[0]:
                 j += 1
                 i = 0
-                
+
     timestamps = []
-    for img, time in db:
-        timestamps.append(time)
-    return timestamps
+    #for img, time in db:
+    #    timestamps.append(time)
+    return db 
 
 print(same_shape(images))
+
+final_timestamps = []
+all_image = same_shapes(images)
+for img, l, timestamp in all_image:
+    new_timestamps = [timestamp]
+    for k in range(3):
+        new_rot = np.rot90(img, k)
+        for sh in all_image:
+            if same_meteorite.same_meteroite(sh) == same_meteorite.same_meteroite(new_rot):
+                new_timestamps.append(sh)
+                break
+    
+    final_timestamps.append(new_timestamps)
--- src/lvl4/rotate.py
+++ src/lvl4/rotate.py
@@ -1,7 +1,6 @@
 import numpy as np
 from same_meteorite import *
 
-def rotate(img):
+def rotate(img, k):
     right = same_meteorite(np.rot90(img, k=1, axes=(0,1)))
-    left = same_meteorite(np.rot90(img, k=1, axes=(1,0))) 
-    return right, left 
+    return right, left, middle 
